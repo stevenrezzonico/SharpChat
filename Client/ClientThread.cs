9ccbf65d52a05cab19132ef7f74bfc4794cdc883
Steven Rezzonico <steven.rezzonico@gmail.com>
2016-03-16 00:36:57 +0100

    Client test

---
+++ Client/ClientThread.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Client {
+    class ClientThread {
+        public bool closed = false;
+        private TcpClient client;
+        private StreamReader streamReader;
+        private StreamWriter ots;
+
+        public ClientThread(TcpClient client, StreamReader streamReader, StreamWriter ots) {
+            this.client = client;
+            this.streamReader = streamReader;
+            this.ots = ots;
+        }
+
+        public void run() {
+            string responseLine;
+            try {
+                while ((responseLine = streamReader.ReadLine()) != null) {
+                    Console.WriteLine(responseLine);
+                    if (responseLine.IndexOf("*** Adios") != -1) {
+                        break;
+                    }
+                }
+                closed = true;
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client {
+    class Program {
+        private static TcpClient client;
+        private static StreamReader streamReader;
+        private static StreamWriter streamWriter;
+
+
+        static void Main(string[] args) {
+            try {
+                client = new TcpClient("127.0.0.1", 7777);
+                streamReader = new StreamReader(client.GetStream());
+                streamWriter = new StreamWriter(client.GetStream());
+                streamWriter.AutoFlush = true;
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+
+            if (client != null && streamWriter != null && streamReader != null) {
+                try {
+                    ClientThread cli = new ClientThread(client, streamReader, streamWriter);
+                    Thread ctThread = new Thread(cli.run);
+                    ctThread.Start();
+
+                    while (!cli.closed) {
+                        string msg = Console.ReadLine().Trim();
+                        streamWriter.WriteLine(msg);
+                    }
+                    streamWriter.Close();
+                    streamReader.Close();
+                    client.Close();
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
